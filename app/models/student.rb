4240f59790e9a7608edb5615e43a4aaa42a988f0
benedicttan <codedivision@11th.ventures>
2015-01-21 19:48:49 +0800

    finished ex 3

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,24 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  def name
+    first_name = self.first_name.to_s
+    last_name = self.last_name.to_s
+    return "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
+#Regex for email stolen from internet
+    validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates :email, :uniqueness => true
+    validates :age, :exclusion => { in: (0..5)}
+#Regex worked out according to error message output in terminal. I.e. the format is "(510) 555-1212 x4567" not as in question..
+    validates :phone, :format => { :with =>  /\(\d{3}\) \d{3}-\d{4}/ }
+
 end
+
+class Teacher < ActiveRecord::Base
--- db/migrate/Create_teachers.rb
+++ db/migrate/Create_teachers.rb
@@ -1,14 +1,10 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to
-
-class CreateStudents < ActiveRecord::Migration
+class CreateTeacher < ActiveRecord::Migration
   def change
-    create_table :students do |t|
+    create_table :teachers do |t|
       t.string :first_name
       t.string :last_name
-      t.string :gender
-      t.date :birthday
       t.string :email
       t.string :phone
       t.timestamps null: false
